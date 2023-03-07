3e7198a0c283ef49c58c1be68fe99e27fef94660
Эмин Назаров <nazarov.emin@outlook.com>
2023-03-07 15:58:10 +0300

    Add event class

---
+++ DailyPlanner/PlannedEvent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlannerCore
+{
+    public class PlannedEvent
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string? Body { get; set; }
+        public DateTime EventStartDateTime { get; set; }
+        public DateTime NotificationDateTime { get; set; } = new();
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime ModifiedDateTime { get; set; }
+
+
+    }
+}
